bcb4c35a9c0a213ff01c83b3a92db418395861bd
hellertang <hellertang@tencent.com>
2024-05-23 16:04:50 +0800

    switch to v1alpha1

--- apis/v1beta1/doc.go
+++ apis/v1beta1/doc.go
@@ -5,5 +5,5 @@ Copyright 2022 Upbound Inc.
 // Package v1beta1 contains the core resources of the tencentcloud upjet provider.
 // +kubebuilder:object:generate=true
 // +groupName=tencentcloud.crossplane.io
-// +versionName=v1beta1
+// +versionName=v1alpha1
 package v1beta1
