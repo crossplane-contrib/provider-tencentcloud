581c9bc5248e77bdcc5e8f329cfd47ad191e40a8
tongyiming <tongyiming@threebody.xyz>
2024-07-08 03:16:21 +0000

    update terraform version
    Signed-off-by: tongyiming <tongyiming@threebody.xyz>

--- apis/tdmq/v1alpha1/zz_tdmqnamespace_types.go
+++ apis/tdmq/v1alpha1/zz_tdmqnamespace_types.go
@@ -170,7 +170,7 @@ type TdmqNamespaceStatus struct {
 // +kubebuilder:subresource:status
 // +kubebuilder:storageversion
 
-// TdmqNamespace is the Schema for the TdmqNamespaces API. Provide a resource to create a tdmq namespace.
+// TdmqNamespace is the Schema for the TdmqNamespaces API. Provide a resource to create a TDMQ namespace.
 // +kubebuilder:printcolumn:name="READY",type="string",JSONPath=".status.conditions[?(@.type=='Ready')].status"
 // +kubebuilder:printcolumn:name="SYNCED",type="string",JSONPath=".status.conditions[?(@.type=='Synced')].status"
 // +kubebuilder:printcolumn:name="EXTERNAL-NAME",type="string",JSONPath=".metadata.annotations.crossplane\\.io/external-name"
